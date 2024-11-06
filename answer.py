c55775664eaaa9570e12143e8d5b656e1eb65793
joebadmus <joebadmus@gmail.com>
2024-11-06 09:53:55 +0000

    Add ansible file

---
+++ answer.py
@@ -0,0 +1,17 @@
+# password = "speech"
+# user_guess = ""
+# count = 1
+
+# while user_guess != password:
+#     user_guess = input("What is your password: ")
+
+#     if user_guess != password:
+#         count += 1
+
+#     else:
+#         print(f"Well done, you took {count} attempts")
+
+
+def med(hello, hi):
+    if hello == num
+    print(hello)
